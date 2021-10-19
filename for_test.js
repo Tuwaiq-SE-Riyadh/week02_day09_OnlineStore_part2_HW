0b710a06cc224c34c5d406d1cd98363a3c7cd132
aaldosari1 <dosr@live.com>
2021-10-19 09:12:48 +0300

    was not lucky with this one but I learned a lesson

---
+++ for_test.js
@@ -0,0 +1,34 @@
+
+const beba = [
+    {
+     itemName: "Android Phone",
+      price: 400,
+    },
+    {
+      itemName: "Watch",
+      price: 100,
+    },
+    {
+      itemName: "Apple pie",
+      price: 20,
+    },
+    {
+      itemName: "Iphone",
+      price: 1000,
+    },
+    {
+      itemName: "Perfume",
+      price: 50,
+    },
+    {
+      itemName: "Ice cream",
+      price: 12,
+    },
+  ];
+shop=[]
+function t(){
+  //  let s=document.getElementsByClassName('btn btn-primary')[4]
+//shop.push()
+console.log(beba[0])
+
+}
